c2f3ec1b4a8b9408ceeca118b0ea0e4ae50eb577
Boris Tane <boris.tane@gmail.com>
2020-01-16 22:18:26 +0000

    Fix test failing at night

--- test/unit/activation-tokens.test.ts
+++ test/unit/activation-tokens.test.ts
@@ -1,4 +1,27 @@
 import { createToken } from "../../src/utils/activation-tokens"
+import { promisify } from "util";
+import setupDb from "../utils/setupDb";
+import { createConnectionToDB } from "../../src/utils/db-helper";
+import tokens from "../data/activation-tokens.json";
+import users from "../data/users.json";
+import { removeAllFromDB } from "../utils/removeFromDb";
+import { insertActivationTokens, ITestActivationToken, insertUsers, ITestUser } from "../utils/insertToDb";
+import moment from "moment";
+
+let sleep = promisify(setTimeout);
+let connection;
+
+beforeAll(async () => {
+  await setupDb();
+  sleep(2000);
+  connection = await createConnectionToDB();
+});
+
+beforeEach(async () => {
+  await removeAllFromDB();
+  await insertUsers(users as ITestUser[]);
+  await insertActivationTokens(tokens as ITestActivationToken[]);
+});
 
 describe("Activation token generation", () => {
   it("Generates a valid token string", () => {
@@ -11,6 +34,6 @@ describe("Activation token generation", () => {
   it("Generates a token that expires in two hours", () => {
     const result = createToken();
     const now = new Date();
-    expect(result.expires.getHours()).toEqual(now.getHours() + 2);
+    expect(result.expires.getHours()).toEqual(moment(now).add(2, "hours").hours());
   });
 });
