708817a0c27786e32134b9162758fdb0321df5cf
Boris Tane <boris.tane@gmail.com>
2020-01-19 00:36:00 +0000

    Adds route to get internal email

--- src/controller/internal.ts
+++ src/controller/internal.ts
@@ -48,3 +48,46 @@ export async function getUser(req: Request, res: Response, next: NextFunction) {
     return next();
   }
 }
+
+export async function getUserEmail(req: Request, res: Response, next: NextFunction) {
+  const correlationId = res.get("x-correlation-id") || "";
+  try {
+    const api = res.locals.apiData as APIService;
+    if (api.name !== "email-service" || !api.isActive || moment(api.expires).isBefore(moment.now())) {
+      send401(res, { message: "Unauthorized operation" });
+      return next();
+    }
+    const { uuid } = req.params;
+    const user = await getRepository(User).findOne({ uuid });
+    if (!user) {
+      send404(res, { message: "User not found" });
+      return next();
+    }
+
+    const response = {
+      message: "User found.",
+      user: {
+        email: user.email,
+      },
+      request: {
+        type: "GET",
+        url: `/internal/users/`
+      }
+    };
+    api.lastUsed = new Date();
+    await getRepository(APIService).save(api);
+    res.locals.body = response;
+    res.status(200).json(response);
+    next();
+  } catch (err) {
+    const message = "Unexpected error when getting user";
+    logger.error({
+      message,
+      data: req.body,
+      error: err,
+      correlationId,
+    });
+    send500(res, { message }, err);
+    return next();
+  }
+}
