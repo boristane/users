4003366445aa8f15a56b672de719309176ea5bd5
Boris Tane <boris.tane@gmail.com>
2020-04-28 21:07:53 +0100

    adds the make premium route

--- src/controller/internal.ts
+++ src/controller/internal.ts
@@ -5,11 +5,13 @@ import logger = require("logger");
 import { send404, send500, send401 } from "../utils/http-error-responses";
 import { APIService } from "../entity/APIService";
 import moment = require("moment");
+import { Membership } from "../entity/Membership";
+import { MembershipTier } from "../utils/utils";
 
 export async function getUser(req: Request, res: Response, next: NextFunction) {
   try {
     const api = res.locals.apiData as APIService;
-    if (!api.isActive || moment(api.expires).isBefore(moment.now())) {
+    if (!api.isActive) {
       send401(res, { message: "Unauthorized operation" });
       return next();
     }
@@ -43,10 +45,57 @@ export async function getUser(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+export async function makePremium(req: Request, res: Response, next: NextFunction) {
+  try {
+    const api = res.locals.apiData as APIService;
+    if (!api.isActive) {
+      send401(res, { message: "Unauthorized operation" });
+      return next();
+    }
+    const { uuid } = req.params;
+    const user = await getRepository(User).findOne({ uuid }, { relations: ["memberships"] });
+    if (!user) {
+      send404(res, { message: "User not found" });
+      return next();
+    }
+
+    const membership: Membership = {
+      tier: MembershipTier.premium,
+      expirationDate: moment(new Date()).add(3, "month").toDate(),
+      startDate: new Date(),
+      isActive: true,
+    }
+    user.memberships.forEach(m => m.isActive = false);
+    user.memberships.push(membership);
+    await getRepository(User).save(user);
+
+    const response = {
+      message: "Membership updated.",
+      user: {
+        uuid: user.uuid,
+      },
+      request: {
+        type: "GET",
+        url: `/internal/users/`
+      }
+    };
+    api.lastUsed = new Date();
+    await getRepository(APIService).save(api);
+    res.locals.body = response;
+    res.status(200).json(response);
+    next();
+  } catch (err) {
+    const message = "Unexpected error when getting user";
+    logger.error(message, { data: req.body, error: err, })
+    send500(res, { message }, err);
+    return next();
+  }
+}
+
 export async function getUserEmail(req: Request, res: Response, next: NextFunction) {
   try {
     const api = res.locals.apiData as APIService;
-    if (api.name !== "email-service" || !api.isActive || moment(api.expires).isBefore(moment.now())) {
+    if (api.name !== "email-service" || !api.isActive) {
       send401(res, { message: "Unauthorized operation" });
       return next();
     }
--- src/router/internal.ts
+++ src/router/internal.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-  getUser, getUserEmail,
+  getUser, getUserEmail, makePremium,
 } from "../controller/internal";
 
 const router = express.Router();
 
 router.get("/users/:uuid", getUser);
+router.put("/users/make-premium/:uuid", makePremium);
 router.get("/users/email/:uuid", getUserEmail);
 
 export default router;
